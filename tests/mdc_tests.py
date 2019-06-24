ad8cd3664ec93e13e725215d0679ac038a485860
Stepland <16676308+Stepland@users.noreply.github.com>
2019-06-24 18:43:33 +0200

    Add a bunch of tests

--- tests/mdc_tests.py
+++ tests/mdc_tests.py
@@ -1,6 +1,13 @@
 import logging
 
-from mdc import with_mdc, MDC
+from mdc import with_mdc, MDC, log_value, new_log_context, log_argument
+
+
+def assert_context_has(**kwargs):
+    record = logging.makeLogRecord({})
+    for key, value in kwargs.items():
+        assert hasattr(record, key)
+        assert getattr(record, key) == value
 
 
 def test_attaching_fields():
@@ -30,3 +37,66 @@ def test_mdc_decorator():
         assert ctx.baz == "bar", ctx.baz
 
     foobar(1)
+
+
+def test_generator():
+    @log_value(inside=True)
+    def my_generator():
+        for i in range(5):
+            record = logging.makeLogRecord({})
+
+            assert hasattr(record, "inside")
+            assert record.inside == True
+
+            yield i
+
+    with new_log_context(inside=False) as ctx:
+        for _ in my_generator():
+            assert ctx.inside == False
+
+
+def test_log_value():
+    @log_value(foo="bar")
+    def my_function():
+        assert_context_has(foo="bar")
+
+    assert not hasattr(logging.makeLogRecord({}), "foo")
+    my_function()
+    assert not hasattr(logging.makeLogRecord({}), "foo")
+
+
+def test_nested_contexts():
+
+    with new_log_context(food="spam"):
+        with new_log_context(food="ham"):
+            with new_log_context(food="eggs"):
+                assert_context_has(food="eggs")
+            assert_context_has(food="ham")
+        assert_context_has(food="spam")
+
+
+def test_log_argument():
+    @log_argument("food")
+    def eat(food):
+        assert_context_has(food=food)
+
+    for food in ["spam", "ham", "eggs"]:
+        eat(food)
+
+
+def test_log_argument_with_destination():
+    @log_argument("food", "abcd")
+    def eat(food):
+        assert_context_has(abcd=food)
+
+    for food in ["spam", "ham", "eggs"]:
+        eat(food)
+
+
+def test_log_argument_with_formatter():
+    @log_argument("food", formatter=str.upper)
+    def eat(food):
+        assert_context_has(food=str.upper(food))
+
+    for food in ["spam", "ham", "eggs"]:
+        eat(food)
